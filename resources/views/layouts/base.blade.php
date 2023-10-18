f9cbdc738957573ea50d868c493623494466ab98
Ruan Felipe <ruansilva6721@gmail.com>
2023-10-18 10:23:08 -0300

    hotfix: changing tailwind route

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -18,7 +18,7 @@
     <link rel="stylesheet" href="{{ asset('js/script.js') }}">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-    @vite('resources/css/app.css')
+    <script src="https://cdn.tailwindcss.com"></script>
     @stack('style')
 
     <title>{{ env('APP_NAME') }}</title>
